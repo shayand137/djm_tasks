c071a496f6d184fea5c27f55bc66d5a437c76a99
shayand137 <112331967+shayand137@users.noreply.github.com>
2023-02-08 15:47:22 +0200

    contactus

--- layout/contactus.php
+++ layout/contactus.php
@@ -3,5 +3,4 @@ $tite = "Contact US - hamk Oy";
 include "header.php" ?>
 <h2> Contact US <h2>
     <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga ipsa saepe quis temporibus, corporis adipisci. Hic, placeat aut earum culpa modi obcaecati veritatis totam sit at ut dolorem ea ipsa.</p>
-
     <?php include "footer.php" ?>
