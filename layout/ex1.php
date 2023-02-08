4664c60f17fc89e01b7c27e5797d3afb401a12e4
shayand137 <112331967+shayand137@users.noreply.github.com>
2023-02-08 15:49:32 +0200

    ex1

--- layout/ex1.php
+++ layout/ex1.php
@@ -1,9 +1,11 @@
 <?php include "header.php" ?>
+
 <h3>3.1.Write a simple PHP script to print your information (Name and your groupid). </h3>
 <?php
 echo "mohammadreza dejamfard<br>";
 echo "BBCAP22";
 ?> 
+
 <h3>3.2.Write PHP code to display the following message.</h3>
 <?php
 echo "Hello world! My name is \"David\"";
@@ -13,12 +15,12 @@ echo "Hello world! My name is \"David\"";
 echo date("d.m.Y");
 ?> 
 
-
 <h3> 3.4.$title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document. </h3>
 <?php
 $title1 = "php in interesting";
 echo "<h1>" . $title1 . "</h1>";
 ?>
+
 <h3> 3.5.table & variable </h3>
 <?php
 $g1 = 5;
@@ -41,6 +43,7 @@ echo "
 </table>
 ";
 ?>
+
 <h3> 4 screenshot of the development environment </h3>
 <img scr="sc.jpg" alt ="sc4">
 <?php include "footer.php"
