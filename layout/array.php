9e718c78d707ee31be239d0a4bc4dee3e7fe661c
shayand137 <112331967+shayand137@users.noreply.github.com>
2023-02-09 11:11:59 +0200

    2

--- layout/array.php
+++ layout/array.php
@@ -8,6 +8,15 @@ foreach ($courses as $i){
     echo "$i";
 }
 ?>
+<h4>2. The unset() function is used to remove element from the array. The var_dump() function is used to dump information about a variable.  array_values() is an inbuilt function that returns all the values of an array and not the keys.
+Delete an element from the array below:
+$courses1=array("PHP", "HTML", "JavaScript", "CMS", "Project");</h4>
+
+<?php
+$courses1 = array("PHP", "HTML", "JavaScript", "CMS", "Project");
+unset($courses1[4]);
+echo implode(",", $courses1);
+?>
 
 
 
